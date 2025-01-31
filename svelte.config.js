e117d2d0c7a899db52f30a7cf86aaba326a670b0
Ken Power <github@kenpower.com>
2025-01-31 12:05:07 +0000

    fix root path in pages 4

--- svelte.config.js
+++ svelte.config.js
@@ -20,7 +20,7 @@ const config = {
 			handleHttpError: 'warn'
 		},
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/STARS_blog' : '',
+			// base: process.env.NODE_ENV === 'production' ? '/STARS_blog' : '',
 		}
 
 	},
